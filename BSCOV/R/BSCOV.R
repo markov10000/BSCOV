87cc5e2c914b0b3528274a9585a2aa1ac9dfae00
markov10000 <53785868+markov10000@users.noreply.github.com>
2021-10-23 23:14:09 +0100

    Add files via upload

--- BSCOV/R/BSCOV.R
+++ BSCOV/R/BSCOV.R
@@ -4,17 +4,17 @@
 #' @param normalisation if \eqn{normalisation=TRUE}, normalized the input data
 #' @param r the number of factors, if \eqn{r=NULL}, screening over a range of factor number candidates is performed as described in the paper
 #' @param bn.op an index number for the information criterion-based estimator of Bai and Ng (2002) for the number of factors
-#' @param sig.lev not used now
+#' @param sig.lev
 #' @param dw trims off the interval of consideration in the binary segmentation algorithm and determines the minimum length of a stationary segment; if dw=NULL, a default value is chosen as described in the Appendix of Barigozzi, Cho & Fryzlewicz (2016)
 #' @param rule the depth of a binary tree for change-point analysis, see the Appendix of Barigozzi, Cho & Fryzlewicz (2016)
 #' @param SN.op if \eqn{SN.op==1} Self Normalized; \eqn{SN.op==2} mean of absolute; \eqn{SN.op==3} \eqn{mad(abs(diff(x)-mad(diff(x))))} ...
 #' @param refact.op if \eqn{refact.op=TRUE}, redo the PCA on each segment after the breaks in the factors are found
-#' @param common.norm norm used in the  CUSUM statistics aggregation of factors
+#' @param common.norm norm used in the  CUSUM statistics aggregation of factors 
 #' @param idio.norm norm used in the  CUSUM statistics aggregation of idiosyncratic errors
-#' @param common.norm.thr threshold used in the CUSUM statistics of factors
+#' @param common.norm.thr threshold used in the CUSUM statistics of factors 
 #' @param idio.norm.thr threshold used in the CUSUM statistics of idiosyncratic errors
 #' @param norm.op if \eqn{idio.diag=TRUE} the threshold is multiplied by \eqn{((e-s+1)/T)^0.5}
-#' @param SSIC.slack positive constant used in the SSIC
+#' @param SSIC.slack positive constant used in the SSIC 
 #' @param max.q the maximum number of factors, if \eqn{max.q=NULL}, a default value is chosen as described in the paper
 #' @param WBS Wild Binary Segmentation
 #' @param SBS Sparsified Binary Segmentation
@@ -23,7 +23,7 @@
 ##' @param idio.diag if \eqn{idio.diag=TRUE}, only the diagonal wavelet-transform is employed in order to generate the panel of statistics from the idiosyncratic components
 #' @param do.parallel if \eqn{do.parallel=TRUE}, a set of copies of R running in parallel are created and used for bootstrap procedure
 #' @param no.proc sets the number of processes to be spawned when do.parallel=TRUE
-#' @param setseed sets the ramdon seed
+#' @param setseed sets the ramdon seed 
 #' @return change points
 #' \itemize{
 #'  \item{"gfm"}{information of factor model}
@@ -35,13 +35,12 @@
 #'  \item{"idio.est.cps"}{location idiosyncratic component breaks}
 #' }
 #' @author Yu-Ning Li \email{yl3409@york.ac.uk}
-#' @export
 BSCOV <-
 function (x,normalisation=FALSE, r = NULL, bn.op = 2, sig.lev=1, dw = NULL, rule = NULL, SN.op=0, refact.op=0,
       common.norm=2, idio.norm=2, common.norm.thr=0, idio.norm.thr=Inf, norm.op=FALSE, SSIC.slack=c(1/2, 1/2),
      max.q = NULL, WBS=1, SBS=1, M=500,  input.op=1,
-     idio.diag = FALSE,
-    do.parallel = TRUE, no.proc = 2, setseed=0)
+     idio.diag = FALSE, 
+    do.parallel = TRUE, no.proc = 2, setseed=0) 
 {
 
 if(input.op==3){#wavelet
@@ -52,25 +51,25 @@ if(input.op==3){#wavelet
     n <- nrow(x)
     T <- ncol(x)
     if (length(SSIC.slack)==1) SSIC.slack = rep(SSIC.slack,2)
-    if (is.null(max.q))
+    if (is.null(max.q)) 
         max.q <- max(round(20, sqrt(min(n, T))))
-    if (is.null(dw))
+    if (is.null(dw)) 
         dw <- round(min(log(T)^2, T^(6/7)/4))
 
     if(is.null(rule)) rule <- round(log(T, 2)/2)
     if(idio.norm.thr==0)SBS=0
 
 
-    para<-list(M=M, SN.op=SN.op, is.NoF.fixed=r, max.q=max.q, sig.lev=sig.lev, dw=dw, rule=rule, normalisation=normalisation, refact.op=refact.op,
-        common.norm=common.norm, idio.norm=idio.norm,idio.norm.thr=idio.norm.thr,norm.op=norm.op, SSIC.slack=SSIC.slack,
-        idio.diag =idio.diag, WBS=WBS,SBS=SBS,input.op=input.op,
+    para<-list(M=M,r=r, SN.op=SN.op, is.NoF.fixed=r, max.q=max.q, sig.lev=sig.lev, dw=dw, rule=rule, normalisation=normalisation, refact.op=refact.op,
+        common.norm=common.norm, idio.norm=idio.norm,idio.norm.thr=idio.norm.thr,norm.op=norm.op, SSIC.slack=SSIC.slack, 
+        idio.diag =idio.diag, WBS=WBS,SBS=SBS,input.op=input.op, 
         do.parallel=do.parallel, no.proc = no.proc, setseed=setseed)
 
     gfm0 <- get.fac.mod(x, bn.op = bn.op, max.q = max.q, normalisation=normalisation)
     x <- gfm0$norm.x
-    if (is.null(r))
+    if (is.null(r)) 
         q.hat <- gfm0$q.hat
-    else q.hat <- r # given NoF
+    else q.hat <- r # given NoF 
 
     if (do.parallel) {
         cl.cores <- parallel::detectCores()
@@ -84,12 +83,12 @@ if(input.op==3){#wavelet
             WBS=WBS, M=M, input.op=input.op,
             do.parallel = do.parallel, setseed=setseed)
         common.est.cps <- sort(cs$est.cps)
-        hat.vep<-get.residual(gfm0=gfm0, common.est.cps=common.est.cps, refact=refact.op,bn.op = bn.op)
+        hat.vep<-get.residual(gfm0=gfm0, r=q.hat, common.est.cps=common.est.cps, refact=refact.op,bn.op = bn.op)
    }else{#NoF=0
         cs <- NULL
         common.est.cps <- as.numeric()
         hat.vep <- x
-   }
+   } 
     is <- idio.seg(hat.vep, dw = dw, rule = rule,  SN.op=SN.op, sig.lev=sig.lev,
         idio.norm=idio.norm, idio.norm.thr=idio.norm.thr, norm.op=norm.op, SSIC.slack=SSIC.slack,
          WBS=WBS, SBS=SBS, M=M, idio.diag=idio.diag, input.op=input.op,
@@ -98,6 +97,6 @@ if(input.op==3){#wavelet
 
      idio.est.cps <- is$est.cps
     if (do.parallel) parallel::stopCluster(cl)
-return(list(gfm = gfm0, para=para, hat.vep=hat.vep, common.seg.res=cs, common.est.cps = common.est.cps,
+return(list(gfm = gfm0, para=para, hat.vep=hat.vep, common.seg.res=cs, common.est.cps = common.est.cps, 
         idio.seg.res = is, idio.est.cps = idio.est.cps))
 }
